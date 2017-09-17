9d2f64d0516d11cb5f560e14aa8c68df878e9523
tasi788 <tasi788@gmail.com>
2017-09-17 15:24:59 +0800

    gifs

--- assets/js/pic.js
+++ assets/js/pic.js
@@ -1,8 +1,7 @@
-var imagesArray = ['giphy.gif', '1banner.jpg'];
-
 function displayImage(){
-		var len = imagesArray.length;
-    var num = Math.floor(Math.random() * len);
-		var imgStr = '<section id="banner" class="bg-img" data-bg="' + imagesArray[num] +'">';
+		//var imagesArray = ['giphy.gif', '1.gif'];
+		//var len = imagesArray.length;
+    var num = Math.floor(Math.random() * 5);
+		var imgStr = '<section id="banner" class="bg-img" data-bg="' + num +'.gif">';
 		document.write(imgStr); document.close();
 }
